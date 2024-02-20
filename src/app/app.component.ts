a8afecf3b86576360de75422fd355746e38998f6
Douglas Pardim <douglas.skd@gmail.com>
2024-02-19 21:05:53 -0300

    feat: add all feature changes, included: logo and custom data loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-blog';
+  currentDate = new Date().getFullYear()
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,24 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+interface News {
+  id: string;
+  title: string;
+  description: string;
+  photoCover: string;
+  contentData: string;
+}
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
-
   @Input()
-  photoCover:string = ""
+  news: News | undefined;
 
-  @Input()
-  cardTitle:string = ""
-
-  @Input()
-  Id:string="0"
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
-
+  
 }
